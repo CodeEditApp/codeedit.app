926f4d96aa8337a4b69d3684ca05038088e70e43
Austin Condiff <austin.condiff@gmail.com>
2022-04-04 14:51:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vercel.com/?utm_source=codeedit&utm_campaign=oss"
           target="_blank"
           rel="noopener noreferrer"
         >
