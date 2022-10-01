bdc6fed0c59f5d444ec667be2e114f340a0dd5a6
Austin Condiff <austin.condiff@gmail.com>
2022-10-01 13:51:52 -0500

    Added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -34,6 +34,35 @@ export default class MyDocument extends Document {
         <Head>
           <meta charset="utf-8" />
           <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+          <meta
+            property="og:title"
+            content="The Portfolio of Austin Condiff - UX/UI Designer & Developer"
+          ></meta>
+          <meta
+            property="og:image"
+            content="http://codeedit.app/assets/social-preview.jpg"
+          ></meta>
+          <meta
+            property="og:description"
+            content="I design and develop beautiful yet functional interactive user experiences that provide both customer value and product success."
+          ></meta>
+          <meta property="og:url" content="http://codeedit.app"></meta>
+          <meta
+            name="twitter:image"
+            content="https://codeedit.app/assets/social-preview.jpg"
+          ></meta>
+          <meta
+            name="twitter:image:src"
+            content="https://codeedit.app/assets/social-preview.jpg"
+          ></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:site" content="@CodeEditApp"></meta>
+          <meta name="twitter:creator" content="@CodeEditApp"></meta>
+          <meta name="twitter:title" content="CodeEdit for macOS"></meta>
+          <meta
+            name="twitter:description"
+            content="CodeEdit, a lightweight, natively built editor for macOS. Open source. Free forever."
+          ></meta>
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         </Head>
         <body>
